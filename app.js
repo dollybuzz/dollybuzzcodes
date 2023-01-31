82ffb2298a86646e6235b4c82bd7b067171a9272
Dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2023-01-31 12:00:24 +0100

    running server on http instead of express library

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const app = express();
 
 //routes
@@ -9,4 +9,20 @@ app.get("/", function(req,res){
 //server listener
 app.listen("8081", "127.0.0.1", function() {
     console.log("Local express Server is running...");
-})
+})*/
+
+//Creating server according to node.js guide, ref: https://nodejs.org/en/docs/guides/getting-started-guide/
+const http = require('http');
+const port = 8081;
+const host = '127.0.0.1';
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, host, () => {
+    console.log(`Server running locally at http://${host}:${port}/`);
+});
+

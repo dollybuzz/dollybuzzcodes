6d606be821bca5e342a8babd6164c312d3b3a5de
Dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2023-01-31 13:32:45 +0100

    practice creating GET routes on express and HTTP

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-/*const express = require("express");
+//Running server on express
+const express = require("express");
 const app = express();
 
 //routes
@@ -6,23 +7,52 @@ app.get("/", function(req,res){
     res.send("working well");
 });
 
-//server listener
-app.listen("8081", "127.0.0.1", function() {
-    console.log("Local express Server is running...");
-})*/
+app.get("/main", function(req,res) {
+    res.send("another route");
+});
 
-//Creating server according to node.js guide, ref: https://nodejs.org/en/docs/guides/getting-started-guide/
+//server listener
+app.listen("8082", "127.0.0.1", function() {
+    console.log("Local Express Server is running...");
+});
+
+//Running server on http as suggested on node.js guide, ref: http6s://nodejs.org/en/docs/guides/getting-started-guide/
 const http = require('http');
+const url = require('url');
 const port = 8081;
 const host = '127.0.0.1';
 
 const server = http.createServer((req,res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.setHeader('Content-Type', 'text/html');
+
+//error handling
+req.on('error', err => {
+    console.error(err);
+    res.statusCode = 400;
+    res.end('400: Bad request');
+    return;
 });
 
+//routes
+const parsed = url.parse(req.url, true); //parse the request url
+const reqPath = parsed.pathname; //get path from the parsed url
+
+if (req.method == "GET") {
+    if(reqPath == "/main") {
+        res.write(`Hello, you are visiting http://${host}:${port}` + parsed.href);
+        res.end();
+    }
+    else 
+    {
+        res.write("Hello world");
+        res.end();
+    }
+}
+
+});
+
+//server listener
 server.listen(port, host, () => {
-    console.log(`Server running locally at http://${host}:${port}/`);
+    console.log(`HTTP Server running locally at http://${host}:${port}/`);
 });
-
